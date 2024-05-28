b9d9e6e7b023a19e3cf9eed95c53e5b8edcfddb4
usjamil <usjamil@squaretrade.com>
2024-05-28 22:31:30 +0500

    py_client basic setup

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,5 +2,5 @@ import requests
 
 endpoint = "https://www.httpbin.org"
 
-get_response = requests.get(endpoint) # HTTP request
-print(get_response)
+get_response = requests.get(endpoint,verify=False) # HTTP request
+print(get_response.status_code) #printing raw text response or source code
