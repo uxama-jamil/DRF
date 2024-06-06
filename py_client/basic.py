39b851acf9dbf3e47da26428e7d0fcf445d9358b
usjamil <usjamil@squaretrade.com>
2024-06-06 12:47:24 +0500

    empty commit

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,3 +4,4 @@ endpoint = "https://www.httpbin.org"
 
 get_response = requests.get(endpoint,verify=False) # HTTP request
 print(get_response.status_code) #printing raw text response or source code
+
